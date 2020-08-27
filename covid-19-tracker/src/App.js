9bfe37b86fc2846328c14488a396132149d7a3cd
Qandeel Gul <Qandeel@Mardan-MacBook-Pro.local>
2020-08-27 15:33:40 -0400

    created a boiler plate

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import { Cards, Charts, CountryPicker } from './components';
 
 class App extends React.Component{
   render(){
   return (
     <div className="App">
       <h1>App</h1>
+      <Cards />
+      <CountryPicker />
+      <Charts />
+      
     </div>
   );
 }
