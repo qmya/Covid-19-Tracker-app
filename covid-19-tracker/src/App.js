298695be5426f769f160f9e123f3c5d316589000
Qandeel Gul <Qandeel@Mardan-MacBook-Pro.local>
2020-08-27 17:07:26 -0400

    fetch data from api and used

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import './App.scss';
+import { fetchData } from './api';
 import { Cards, Charts, CountryPicker } from './components';
 
 class App extends React.Component{
+  state = {
+    data : {},
+  }
+
+
+ async componentDidMount(){
+   const fetchedData = await fetchData();
+   this.setState({ data : fetchedData });
+   
+ }
+
+
   render(){
+    const { data } = this.state;
+
   return (
-    <div className="App">
+    <div className="container">
       <h1>App</h1>
-      <Cards />
+      <Cards data = {data} />
       <CountryPicker />
       <Charts />
       
