032f45010ac87ac6d4c260353cd24878d106f9f5
Qandeel Gul <Qandeel@Mardan-MacBook-Pro.local>
2020-08-28 15:54:40 -0400

    added a form to pick an country from option and see the covid cases

--- covid-19-tracker/src/components/CountryPicker/CountryPicker.js
+++ covid-19-tracker/src/components/CountryPicker/CountryPicker.js
@@ -3,7 +3,7 @@ import {NativeSelect, FormControl, StylesProvider } from '@material-ui/core';
 import styles from './CountryPicker.module.css';
 import { fetchCountries } from '../../api';
 
-const CountryPicker =() => {
+const CountryPicker =({ handleCountryChange }) => {
 const [fetchedCountries, setFetchedCountries] = useState([]);
     useEffect(() => {
         const fetchAPI = async () => {
@@ -15,7 +15,7 @@ const [fetchedCountries, setFetchedCountries] = useState([]);
     console.log(fetchCountries)
     return (
         <FormControl className= {styles.FormControl}>
-            <NativeSelect>
+            <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
                 <option value="global"> Global </option>
     {fetchedCountries.map(( country, i ) => <option key={i} value= {country}>{country}</option>)}
             </NativeSelect>
